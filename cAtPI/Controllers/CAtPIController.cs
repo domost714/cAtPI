eff490ca2d2b633a47655dd8ffc419d20326ff48
domost714 <dominik.ostrzechowski@interia.pl>
2023-11-28 13:20:22 +0100

    add POST endpoint

--- cAtPI/Controllers/CAtPIController.cs
+++ cAtPI/Controllers/CAtPIController.cs
@@ -1,6 +1,8 @@
 ﻿using cAtPI.Data;
 using cAtPI.Models.DTO;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using System.Data;
 
 namespace cAtPI.Controllers
 {
@@ -9,14 +11,43 @@ namespace cAtPI.Controllers
     public class CAtPIController : ControllerBase
     {
         [HttpGet]
-        public IEnumerable<CatDTO> GetCats()
+        public ActionResult<IEnumerable<CatDTO>> GetCats()
         {
-            return CatBase.catList;
+            return Ok(CatBase.catList);
         }
-        [HttpGet("{id:int}")]
-        public CatDTO GetCats(int id)
+        [HttpGet("{id:int}", Name = "GetCat")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<CatDTO> GetCats(int id)
         {
-            return CatBase.catList.FirstOrDefault(u => u.Id == id);
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+            var cat = CatBase.catList.FirstOrDefault(u => u.Id == id);
+            if (cat == null)
+            {
+                return NotFound();
+            }
+            return Ok(cat);
+        }
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<CatDTO> CreateCat(CatDTO catDTO) 
+        {
+            if (catDTO == null)
+            {
+                return BadRequest(catDTO);
+            }
+            if (catDTO.Id > 0) 
+            {
+                return BadRequest(catDTO);
+            }
+            catDTO.Id = CatBase.catList.OrderByDescending(u => u.Id).FirstOrDefault().Id + 1;
+            CatBase.catList.Add(catDTO);
+            return CreatedAtRoute("GetCat", new { id = catDTO.Id }, catDTO);
         }
     }
 }
